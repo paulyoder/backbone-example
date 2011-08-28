ae05c8bbfa3c5959ce547d0acc9648cfa522ee4c
Paul Yoder <paulyoder@gmail.com>
2011-08-28 16:15:34 -0500

    got collection working

--- scripts/backbone-example.js
+++ scripts/backbone-example.js
@@ -1,19 +1,61 @@
 (function($) {
 
-  window.Detail = Backbone.Model.extend({});
+
+  window.Detail = Backbone.Model.extend({
+    initialize: function() {
+      _.bindAll(this, 'calculateAmount');
+      this.bind('change', this.calculateAmount);
+    },
+
+    calculateAmount: function() {
+      var price = this.get('price');
+      var quantity = this.get('quantity');
+      var amount = price * quantity;
+      this.set({amount: amount});
+    }
+  });
 
   window.DetailView = Backbone.View.extend({
     tagName: 'tr',
 
     initialize: function() {
+      _.bindAll(this, 'render');
+      this.model.bind('change', this.render);
       this.template = _.template($('#row-template').html());
     },
 
     render: function() {
-      var renderedContent = this.template(this.model.toJSON());
-      $(this.el).html(renderedContent);
+      $(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    }
+  });
+ 
+  window.Details = Backbone.Collection.extend({
+    model: Detail  
+  });
+
+  window.DetailsView = Backbone.View.extend({
+    tagName: 'table',
+
+    initialze: function() {
+      _.bindAll(this, 'render');
+    },
+    
+    render: function() {
+      $table = $(this.el);
+      $table.html($('#details-template').html());
+      this.model.each(function(detail) {
+        var detailView = new DetailView({model: detail});
+        $table.append(detailView.render().el);
+      }); 
       return this;
     }
   });
 
 })(jQuery);
+
+$(function() {
+  sample = new Details([{quantity:2,price:3,amount:6},{quantity:4,price:6,amount:24}]);
+  view = new DetailsView({model:sample});
+  $('body').html(view.render().el);
+});
