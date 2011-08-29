084260f4dbfa1fd60d04c9f216a228065991695a
Paul Yoder <paulyoder@gmail.com>
2011-08-28 22:17:19 -0500

    got basic sample working

--- scripts/backbone-example.js
+++ scripts/backbone-example.js
@@ -1,17 +1,22 @@
 (function($) {
 
-
   window.Detail = Backbone.Model.extend({
     initialize: function() {
-      _.bindAll(this, 'calculateAmount');
-      this.bind('change', this.calculateAmount);
+      _.bindAll(this, 'calculateAmount', 'onAmountChanged');
+      this.bind('change:price', this.calculateAmount);
+      this.bind('change:quantity', this.calculateAmount);
+      //this.bind('change:amount', this.onAmountChanged);
+    },
+
+    onAmountChanged: function() {
+      console.log('onAmountChanged');
+      this.set({price: 0, quantity: 0});
     },
 
     calculateAmount: function() {
       var price = this.get('price');
       var quantity = this.get('quantity');
-      var amount = price * quantity;
-      this.set({amount: amount});
+      this.set({amount: (price * quantity)});
     }
   });
 
@@ -19,14 +24,39 @@
     tagName: 'tr',
 
     initialize: function() {
-      _.bindAll(this, 'render');
-      this.model.bind('change', this.render);
+      _.bindAll(this, 'render', 'updateQuantity', 'updatePrice', 'updateAmount', 'inputChanged');
+      this.model.bind('change:quantity', this.updateQuantity);
+      this.model.bind('change:price', this.updatePrice);
+      this.model.bind('change:amount', this.updateAmount);
       this.template = _.template($('#row-template').html());
+    },
+
+    events: {
+      'keyup input': 'inputChanged'
     },
 
     render: function() {
       $(this.el).html(this.template(this.model.toJSON()));
       return this;
+    },
+
+    updateQuantity: function() {
+      $(this.el).find('input.quantity').val(this.model.get('quantity'));
+    },
+    updatePrice: function() {
+      $(this.el).find('input.price').val(this.model.get('price'));
+    },
+    updateAmount: function() {
+      $(this.el).find('input.amount').val(this.model.get('amount'));
+    },
+
+    inputChanged: function(e) {
+      console.log('inputChanged()');
+      var property = $(e.currentTarget).attr('class');
+      var value = $(e.currentTarget).val();
+      var data = {};
+      data[property] = value;
+      this.model.set(data);
     }
   });
  
@@ -44,7 +74,7 @@
     render: function() {
       $table = $(this.el);
       $table.html($('#details-template').html());
-      this.model.each(function(detail) {
+      this.collection.each(function(detail) {
         var detailView = new DetailView({model: detail});
         $table.append(detailView.render().el);
       }); 
@@ -56,6 +86,6 @@
 
 $(function() {
   sample = new Details([{quantity:2,price:3,amount:6},{quantity:4,price:6,amount:24}]);
-  view = new DetailsView({model:sample});
+  view = new DetailsView({collection:sample});
   $('body').html(view.render().el);
 });
