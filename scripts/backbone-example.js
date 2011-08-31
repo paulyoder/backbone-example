7623689c9ae404bc9e66db193ed8b5969f76a36e
Paul Yoder <paulyoder@gmail.com>
2011-08-31 08:34:07 -0500

    added formatNumber function

--- scripts/backbone-example.js
+++ scripts/backbone-example.js
@@ -1,5 +1,31 @@
 (function($) {
 
+  formatNumber = function(number, maxLength, decimalPlaces, allowNegative) {
+    //scrub number
+    number = number.replace(/[^-\.0-9]/g,'')            //only keep numbers, negative and decimal
+                   .replace(/--+/g,'-')                 //delete multiple negatives
+                   .replace(/^(-?\d*\.\d*)\..*$/g,'$1') //only allow one decimal
+                   .replace(/(\d)-/g,'$1')              //delete negatives in middle of number
+                   .replace(/^(-?)0+(\w)/g,'$1$2');     //delete leading zeros but allow single 0
+
+    if (maxLength > 0) { 
+      //make sure there are not more numbers than the maxLength
+      var regEx = new RegExp("^(-?\\d{" + maxLength + "})\\d*(\\.\\d*)?$", 'g');
+      number = number.replace(regEx,'$1$2'); 
+    }
+
+    if (!allowNegative) { number = number.replace(/-/g,''); }
+
+    if (decimalPlaces == 0) { number = number.replace(/\.\d+$/g,''); }
+    else { 
+      var regEx = new RegExp("(\\.\\d{" + decimalPlaces + "})\\d+$", 'g');
+      number = number.replace(regEx,'$1');
+    }
+
+    //add commas before returning
+    return number.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'); 
+  };    
+
   window.Detail = Backbone.Model.extend({
     initialize: function() {
       _.bindAll(this, 'calculateAmount', 'keyup');
